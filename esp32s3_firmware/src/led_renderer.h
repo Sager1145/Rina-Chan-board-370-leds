bd3a192bd39e76c7df8486a0a3688df7db95e31c
Sager1145 <sagerxjp@gmail.com>
2026-06-20 18:16:34 -0400

    Move LED renderer declarations to packed frames

--- esp32s3_firmware/src/led_renderer.h
+++ esp32s3_firmware/src/led_renderer.h
@@ -3,11 +3,7 @@
 #include "config.h"
 #include "state.h"
 
-bool normalizeM370(const String& input, String& normalized, String& error);
-
-bool m370ToPackedBits(const String& input, uint8_t* outBits, String& error);
-
-String blankM370();
+bool validatePackedFrame(const uint8_t* packedBits, String& error);
 
 void setFrameBit(uint16_t index, bool on);
 
@@ -18,17 +14,17 @@ bool packedFrameBit(const uint8_t* bits, uint16_t index);
 uint16_t countLitLeds();
 FrameStateSnapshot readFrameStateSnapshot();
 
-bool applyM370(const String& input, const String& reason, String& error);
+bool applyPackedFrameQueued(const uint8_t* packedBits, const String& reason, String& error);
 
 void applyPackedFrameImmediate(const uint8_t* packedBits, const String& reason);
 
 void applyBlankFrame(const String& reason);
 
-void serviceM370FrameQueue();
+void servicePackedFrameQueue();
 
-void clearQueuedM370Frames();
+void clearQueuedPackedFrames();
 
-uint8_t queuedM370FrameCount();
+uint8_t queuedPackedFrameCount();
 
 void setColorStateNoRender(const String& input);
 
